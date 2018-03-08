8dca00d5165ff79a526b23ccc65f7ab5b24645e2
Vlad Korniev <vkorn@softserveinc.com>
2018-03-08 11:23:37 -0800

    * Licence header

--- src/main/java/eu/openanalytics/domain/Proxy.java
+++ src/main/java/eu/openanalytics/domain/Proxy.java
@@ -1,3 +1,23 @@
+/**
+ * ShinyProxy
+ * <p>
+ * Copyright (C) 2016-2017 Open Analytics
+ * <p>
+ * ===========================================================================
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Apache License as published by
+ * The Apache Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Apache License for more details.
+ * <p>
+ * You should have received a copy of the Apache License
+ * along with this program.  If not, see <http://www.apache.org/licenses/>
+ */
 package eu.openanalytics.domain;
 
 import io.fabric8.kubernetes.api.model.Pod;
